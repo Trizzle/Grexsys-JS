b745e716e011837d42abd0e97cd77e664da1d264
Grexsys <trent@grexsys.com>
2015-11-23 11:42:18 -0600

    Added  a function
    Add a Function

--- grexsys.js
+++ grexsys.js
@@ -1,5 +1,12 @@
 /* by Grexsys, LLC  */
 
+/*   */ 
+/*   */ 
+/*   */ 
+/*   */ 
+/*   */ 
+/*   */ 
+/* =========== Add Commmas =========== */
 function addCommas(nStr)
 {
 	nStr += '';
@@ -11,4 +18,13 @@ function addCommas(nStr)
 		x1 = x1.replace(rgx, '$1' + ',' + '$2');
 	}
 	return x1 + x2;
+}
+/* =========== Format Phone Number =========== */
+function formatPhone(obj) {
+            var numbers = obj.value.replace(/\D/g, ''),
+        	char = {0:'(',3:') ',6:' - '};
+    		obj.value = '';
+    		for (var i = 0; i < numbers.length; i++) {
+        	obj.value += (char[i]||'') + numbers[i];
+    }
 }
