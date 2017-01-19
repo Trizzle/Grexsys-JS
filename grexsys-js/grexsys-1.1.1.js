931e4f3e812f8ed0efe707215a165d641919bb2a
Grexsys <trent@grexsys.com>
2017-01-19 07:17:20 -0600

    Version 1.1
    Updated check password function.

---
+++ grexsys-js/grexsys-1.1.1.js
@@ -0,0 +1,57 @@
+/* by Grexsys, LLC  */
+
+/*   */ 
+/*   */ 
+/*   */ 
+/*   */ 
+/*   */ 
+/*   */ 
+/* =========== Add Commmas =========== */
+/* This function adds a comma between first and second digit such as 1,000 and 1,000,000 */
+function addCommas(nStr)
+{
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+/* =========== Format Phone Number =========== */
+/* This function formats a phone number */
+function formatPhone(obj) {
+            var numbers = obj.value.replace(/\D/g, ''),
+        	char = {0:'(',3:') ',6:' - '};
+    		obj.value = '';
+    		for (var i = 0; i < numbers.length; i++) {
+        	obj.value += (char[i]||'') + numbers[i];
+    }
+}
+
+/* This function is to create a go back link */
+function goBack() {
+  window.history.back()
+}
+/* =========== End go back function =========== */
+function verifyPassMatch(){
+
+var PassOne = document.getElementById("uPass").value;
+var PassTwo = document.getElementById("vPass").value;
+
+if (PassOne.length < 6){
+alert("Password must be at least 6 characters long");
+}
+
+else if (PassOne === PassTwo) {
+alert("Passwords do not match");
+}
+
+else {
+
+}
+
+}
+/* ===========  ================ */
